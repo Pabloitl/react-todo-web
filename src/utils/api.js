f762b7d24df25b17db6eee277980e5bfcad54d6d
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-09-13 15:23:59 -0500

    feat(url): Change api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const { REACT_APP_API_URL } = process.env;
 
 export default axios.create({
-    baseURL: "http://app.pvargas.com:8000/api/",
+    baseURL: "http://app.pvargas.com/api/",
 });
